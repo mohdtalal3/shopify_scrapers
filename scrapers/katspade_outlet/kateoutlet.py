8a2c382937cd8731fcdc321eed66ad1060a8c20b
mohdtalal3 <muhammadtalal21052@gmail.com>
2025-12-11 12:09:49 +0500

    update kate outlet scraper to fix product URL extraction issue

--- scrapers/katspade_outlet/kateoutlet.py
+++ scrapers/katspade_outlet/kateoutlet.py
@@ -89,8 +89,9 @@ async def fetch_product_details(ids_list, batch_size=50):
         browser_type='chromium',
         max_requests_per_crawl=len(urls_to_crawl) + 10,
         request_handler_timeout=timedelta(seconds=60),
-        browser_options={
-            'args': ['--no-sandbox', '--disable-setuid-sandbox']
+        launch_options={
+            'args': ['--no-sandbox', '--disable-setuid-sandbox'],
+            "chromiumSandbox": False
         }
     )
     
